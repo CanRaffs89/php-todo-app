25cabec489e657789f6f4a86185e6a4d6c136ab7
Can <24660563+CanRaffs89@users.noreply.github.com>
2021-01-13 21:53:31 +0000

    Added form validation and created database

--- add.php
+++ add.php
@@ -1,4 +1,30 @@
-<?php ?>
+<?php 
+
+// Initialise empty variables
+$error = $todoText = '';
+
+// Check if any data has been submitted
+if(isset($_POST['submit'])){
+    if(empty($_POST['todo-text'])){
+        // If text field is empty, echo an error
+        $error = 'Please add some text!';
+    } else {
+        // Store inputted text in a new variable
+        $todoText = $_POST['todo-text'];
+        // Check if text is valid - letters, spaces and numbers only
+        if(!preg_match('/^[a-zA-Z0-9\s]+$/', $todoText)){
+            $error = 'Text must be letters and spaces only!';
+        }
+    }
+    if($error){
+
+    } else {
+        // If form is valid, redirect to home page
+        header('Location: index.php');
+    }
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,11 +37,12 @@
                 <div class="card z-depth-3 card-wrapper">
                     <div class="card-content">
                         <h4 class="center heading">Add New Item</h4>
-                        <form action="" class="container">
+                        <form action="add.php" method="POST" class="container">
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input type="text" name="todo-text" placeholder="Type here...">
+                                    <input type="text" name="todo-text" placeholder="Type here..." value="<?php echo htmlspecialchars($todoText) ?>">
                                 </div>
+                                <div class="red-text"><?php echo $error; ?></div>
                                 <div class="input-field col s12 center">
                                     <button class="btn-flat" type="submit" name="submit"><i id="add-icon" class="material-icons">add_circle</i>
                                     </button>
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php ?>
+<?php 
+
+$connect = mysqli_connect('localhost', 'can', 'Rainbow57', 'todo_app');
+if(!$connect){
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,10 +20,6 @@
                         <ul class="collection with-header">
                             <li class="collection-header center"><h4 class="heading">To-Do List</h4><a href="add.php" class="add-icon"><i id="add-icon" class="material-icons">add_circle</i></a></li>
                             <li class="collection-item"><h6 class="list-item">Item 1<a href="#!" class="secondary-content"><i class="material-icons">cancel</i></a></h6></li>
-                            </li>
-                            <li class="collection-item"><h6 class="list-item">Item 2<a href="#!" class="secondary-content"><i class="material-icons">cancel</i></a></h6></li>
-                            </li>
-                            <li class="collection-item"><h6 class="list-item">Item 3<a href="#!" class="secondary-content"><i class="material-icons">cancel</i></a></h6></li>
                             </li>
                         </ul>
                     </div>
