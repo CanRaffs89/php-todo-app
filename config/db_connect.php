8e93c55e2bf6526ed32cb8f5c34152d8bbe1c19a
Can <24660563+CanRaffs89@users.noreply.github.com>
2021-04-22 20:18:30 +0100

    Connected to Heroku DB

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,10 +1,12 @@
-<?php 
-
-// Connect to database
-$connect = mysqli_connect('localhost', 'can', 'Rainbow57', 'todo_app');
-// Output an error if connection fails
-if(!$connect){
-    echo 'Connection error: ' . mysqli_connect_error();
-}
-
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
